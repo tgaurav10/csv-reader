7f7cbecafc3b9451c80bf9a091f67d1e96c2a6e9
Tejasvi Gaurav <tgaurav@tgaurav-mbp15.guidewire.com>
2019-04-15 12:17:34 +0100

    Initial commit

---
+++ uber-application-server/src/main/java/com/hertz/uber/server/UberApplicationServer.java
@@ -0,0 +1,12 @@
+package com.hertz.uber.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UberApplicationServer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UberApplicationServer.class, args);
+    }
+}
