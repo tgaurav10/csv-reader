d36bfa347c69739b9035619767d0e4020c29f960
Tejasvi Gaurav <tgaurav@TGAURAV-MBP15.local>
2019-04-20 13:55:49 +0100

    Adding delete functionality

--- uber-frontend-server/uber-server/src/UberRide.js
+++ uber-frontend-server/uber-server/src/UberRide.js
@@ -2,89 +2,11 @@ import React, {Component} from 'react';
 import axios from "axios";
 import './UberRide.css';
 
-class Page extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {currItems: []};
-    }
-
-    header = ["Start Date", "End Date", "Category", "PickUp Point", "Destination", "Miles", "Purpose"];
-
-    render() {
-        if (this.state) {
-            return (
-                <div>
-                    <table>
-                        <thead>
-                        <tr>
-                            {this.header.map((h, i) => <th key={i}>{h}</th>)}
-                            <th colSpan={2}>Actions</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        {
-                            this.currItems.list.map((data, i) => {
-                                    if (!data.isEditing) {
-                                        return (
-                                            <tr key={i}>
-                                                <td>{data.startDateOfRide}</td>
-                                                <td>{data.endDateOfRide}</td>
-                                                <td>{data.category}</td>
-                                                <td>{data.pickUpPoint}</td>
-                                                <td>{data.destination}</td>
-                                                <td>{data.miles}</td>
-                                                <td>{data.purpose}</td>
-                                                <td className="round">
-                                                    <button onClick={this.props.edit} value={i}>Edit</button>
-                                                </td>
-                                                <td className="round">
-                                                    <button onClick={this.props.delete}>Delete</button>
-                                                </td>
-                                            </tr>
-                                        )
-                                    } else {
-                                        return (
-                                            <tr key={i}>
-                                                <td><input type="datetime-local" value={data.startDateOfRide}
-                                                           onChange={this.props.handleChange} field="startDateOfRide" row={i}/>
-                                                </td>
-                                                <td><input type="datetime-local" value={data.endDateOfRide}
-                                                           onChange={this.props.handleChange} field="endDateOfRide" row={i}/></td>
-                                                <td><input type="text" value={data.category} onChange={this.props.handleChange}
-                                                           field="category" row={i}/></td>
-                                                <td><input type="text" value={data.pickUpPoint} onChange={this.props.handleChange}
-                                                           field="pickUpPoint" row={i}/></td>
-                                                <td><input type="text" value={data.destination} onChange={this.props.handleChange}
-                                                           field="destination" row={i}/></td>
-                                                <td><input type="number" step={0.001} value={data.miles}
-                                                           onChange={this.props.handleChange} field="miles" row={i}/></td>
-                                                <td><input type="text" value={data.purpose} onChange={this.props.handleChange}
-                                                           field="purpose" row={i}/></td>
-                                                <td colSpan={2} className="round">
-                                                    <button onClick={this.props.save} value={i}>Save</button>
-                                                </td>
-                                            </tr>
-                                        )
-                                    }
-
-                                }
-                            )
-                        }
-                        </tbody>
-                    </table>
-                </div>
-            );
-        } else {
-            return <div></div>
-        }
-    }
-}
-
 class UberRide extends Component {
 
     constructor(props) {
         super(props);
-        this.state = {list: [], listChanged: false, pages: 0};
+        this.state = {list: [], listChanged: false};
         this.edit = this.editRow.bind(this);
         this.handleChange = this.handleChangeOnCell.bind(this);
         this.save = this.saveRow.bind(this);
@@ -122,7 +44,6 @@ class UberRide extends Component {
         row[field] = el.value;
         let newList = this.state.list;
         newList[id] = row;
-        console.log(newList);
         this.setState({list: newList});
     }
 
@@ -149,20 +70,27 @@ class UberRide extends Component {
     }
 
     deleteRow(event) {
-        console.log("Deleting");
+        let el = event.target;
+        const id = el.getAttribute('value');
+        let row = this.state.list[id];
+        let curr = this;
+        const uuid = row['uuid'];
+        axios.delete('http://localhost:8080/uber-data/job/' + uuid).then(
+            function (resp) {
+                let newList = curr.state.list;
+                newList.splice(id, 1);
+                curr.setState({list: newList});
+            }).catch(
+            function (error) {
+                console.log(error);
+            }
+        );
     }
 
     header = ["Start Date", "End Date", "Category", "PickUp Point", "Destination", "Miles", "Purpose"];
 
     render() {
         if (this.state) {
-            // const renderPageNumbers = this.state.pages.map(number => {
-            //     return (
-            //         <li key={number} id={number} onClick={this.props.handleClick}>
-            //             {number}
-            //         </li>
-            //     );
-            // });
             return (
                 <table>
                     <thead>
@@ -188,7 +116,7 @@ class UberRide extends Component {
                                                 <button onClick={this.edit} value={i}>Edit</button>
                                             </td>
                                             <td className="round">
-                                                <button onClick={this.delete}>Delete</button>
+                                                <button onClick={this.delete} value={i}>Delete</button>
                                             </td>
                                         </tr>
                                     )
