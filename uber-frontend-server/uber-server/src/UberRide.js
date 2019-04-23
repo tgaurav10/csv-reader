5d3437b58b69d7061a295d3b8936a5678ef80a7f
Tejasvi Gaurav <tgaurav@tgaurav-mbp15.guidewire.com>
2019-04-23 11:49:43 +0100

    Fixing dates

--- uber-frontend-server/uber-server/src/UberRide.js
+++ uber-frontend-server/uber-server/src/UberRide.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import axios from "axios";
 import './UberRide.css';
 import Pagination from "react-paginating";
+import moment from "moment";
 
 class UberRide extends Component {
 
@@ -20,7 +21,12 @@ class UberRide extends Component {
         axios.get('http://localhost:8080/uber-data/job').then(
             function (resp) {
                 let data = resp.data;
-                data.map(row => row['isEditing'] = false);
+                data.map(row => {
+                    row['isEditing'] = false;
+                    row['startDateOfRide'] = curr.formatDateResponse(row['startDateOfRide']);
+                    row['endDateOfRide'] = curr.formatDateResponse(row['endDateOfRide']);
+                    return row;
+                });
                 //console.log(data.length);
                 curr.setState({list: data});
             }).catch(
@@ -30,6 +36,16 @@ class UberRide extends Component {
         );
         curr.state.pages = curr.state.list / 25;
     }
+
+    formatDateResponse(date) {
+        return moment(date.toString(), "MM/DD/YYYY HH:mm").format("YYYY-MM-DDTHH:mm");
+    }
+
+    formatDateInput(date) {
+        return moment(date.toString(), "YYYY-MM-DDTHH:mm").format("MM/DD/YYYY HH:mm");
+    }
+
+
 
     editRow(event) {
         let el = event.target;
@@ -58,10 +74,15 @@ class UberRide extends Component {
         this.setState({listChanged: false});
         let curr = this;
         const uuid = row['uuid'];
+        row['startDateOfRide'] = curr.formatDateInput(row['startDateOfRide']);
+        row['endDateOfRide'] = curr.formatDateInput(row['endDateOfRide']);
         axios.put('http://localhost:8080/uber-data/job/' + uuid, row).then(
             function (resp) {
                 let data = resp.data;
                 data['isEditing'] = false;
+                data['startDateOfRide'] = curr.formatDateResponse(data['startDateOfRide']);
+                data['endDateOfRide'] = curr.formatDateResponse(data['endDateOfRide']);
+                curr.state.list.splice(id, 1);
                 let newList = curr.state.list;
                 newList.push(data);
                 curr.setState({list: newList});
@@ -129,10 +150,10 @@ class UberRide extends Component {
                                                 <td>{data.miles}</td>
                                                 <td>{data.purpose}</td>
                                                 <td className="round">
-                                                    <button onClick={this.edit} value={i}>Edit</button>
+                                                    <button onClick={this.edit} value={(currentPage-1) * limit + i}>Edit</button>
                                                 </td>
                                                 <td className="round">
-                                                    <button onClick={this.delete} value={i}>Delete</button>
+                                                    <button onClick={this.delete} value={(currentPage-1) * limit + i}>Delete</button>
                                                 </td>
                                             </tr>
                                         )
@@ -140,22 +161,22 @@ class UberRide extends Component {
                                         return (
                                             <tr key={i}>
                                                 <td><input type="datetime-local" value={data.startDateOfRide}
-                                                           onChange={this.handleChange} field="startDateOfRide" row={i}/>
+                                                           onChange={this.handleChange} field="startDateOfRide" row={(currentPage-1) * limit + i}/>
                                                 </td>
                                                 <td><input type="datetime-local" value={data.endDateOfRide}
-                                                           onChange={this.handleChange} field="endDateOfRide" row={i}/></td>
+                                                           onChange={this.handleChange} field="endDateOfRide" row={(currentPage-1) * limit + i}/></td>
                                                 <td><input type="text" value={data.category} onChange={this.handleChange}
-                                                           field="category" row={i}/></td>
+                                                           field="category" row={(currentPage-1) * limit + i}/></td>
                                                 <td><input type="text" value={data.pickUpPoint} onChange={this.handleChange}
-                                                           field="pickUpPoint" row={i}/></td>
+                                                           field="pickUpPoint" row={(currentPage-1) * limit + i}/></td>
                                                 <td><input type="text" value={data.destination} onChange={this.handleChange}
-                                                           field="destination" row={i}/></td>
+                                                           field="destination" row={(currentPage-1) * limit + i}/></td>
                                                 <td><input type="number" step={0.001} value={data.miles}
-                                                           onChange={this.handleChange} field="miles" row={i}/></td>
+                                                           onChange={this.handleChange} field="miles" row={(currentPage-1) * limit + i}/></td>
                                                 <td><input type="text" value={data.purpose} onChange={this.handleChange}
-                                                           field="purpose" row={i}/></td>
+                                                           field="purpose" row={(currentPage-1) * limit + i}/></td>
                                                 <td colSpan={2} className="round">
-                                                    <button onClick={this.save} value={i}>Save</button>
+                                                    <button onClick={this.save} value={(currentPage-1) * limit + i}>Save</button>
                                                 </td>
                                             </tr>
                                         )
